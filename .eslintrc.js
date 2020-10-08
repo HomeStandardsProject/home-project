672b52666b16dc254db5d503e947cf3f8bc0ae29
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-10-08 12:25:08 -0400

    add eslint and prettier to project

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint", "prettier", "unused-imports"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "_" }],
+
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+
+    "no-await-in-loop": "off",
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "no-console": "off",
+    "no-continue": "off",
+    "no-restricted-syntax": "off",
+    "no-use-before-define": "off",
+    "max-classes-per-file": "off",
+    "no-underscore-dangle": "off",
+    "vars-on-top": "off",
+    "no-shadow": "off",
+    "no-unused-vars": "off",
+
+    "prettier/prettier": "error",
+
+    "react/prop-types": "off",
+    "react/display-name": "off",
+
+    "unused-imports/no-unused-imports": "error",
+  },
+};
