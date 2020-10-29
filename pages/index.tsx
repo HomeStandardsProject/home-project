f5a830cfac83a144c02df9d57f6cb3e9a44ec9d9
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-10-29 16:33:52 -0400

    adjust content and layout of landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
-import { Button, Flex, Heading } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/core";
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
@@ -8,21 +8,25 @@ const IndexPage = () => (
     <Flex
       justifyContent={"center"}
       minH={`500px`}
+      width={`80%`}
       direction="column"
-      alignItems={"center"}
       bg={"red"}
+      margin={"0 auto"}
     >
-      <Heading as="h1" size="xl" textAlign="center">
-        Title
+      <Heading as="h1" size="xl" textAlign="left" width="80%">
+        Start a free home assessment
       </Heading>
-      <Heading as="h2" size="lg" textAlign="center">
-        Subtitle
-      </Heading>
-      <Link href="/home-assessment">
-        <Button marginTop={"20pt"} variantColor="green">
-          Start your home assessment
-        </Button>
-      </Link>
+      <Text textAlign="left">
+        See if your student housing situation is in breach of any housing
+        bylaws.
+      </Text>
+      <Box>
+        <Link href="/home-assessment">
+          <Button marginTop={"20pt"} variantColor="green">
+            Start now
+          </Button>
+        </Link>
+      </Box>
     </Flex>
   </Layout>
 );
