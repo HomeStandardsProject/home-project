60ed23ad57b438a5daa4983a96f8ea26d1a5b9b5
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-10-30 19:17:36 -0400

    api also uses questions from airtable

--- api/utils/fetchQuestionsBasedOnEnv.ts
+++ api/utils/fetchQuestionsBasedOnEnv.ts
@@ -1,10 +1,26 @@
-import Airtable from "airtable";
 import { AirtableStore } from "../datastore/Airtable";
 import QuestionsData from "../../data/kingston/questions.json";
 import {
   RoomAssessmentQuestion,
   RoomTypes,
 } from "../../interfaces/home-assessment";
+
+// Variable is set at build time when `fetchQuestionsBasedOnEnv` is called
+// from the getStaticProps
+let _QUESTIONS:
+  | {
+      [type in RoomTypes]: RoomAssessmentQuestion[];
+    }
+  | undefined;
+
+export async function getStaticQuestions() {
+  if (_QUESTIONS) {
+    return _QUESTIONS;
+  }
+  const questions = await fetchQuestionsBasedOnEnv();
+  _QUESTIONS = questions;
+  return questions;
+}
 
 export async function fetchQuestionsBasedOnEnv() {
   let questions: { [type in RoomTypes]: RoomAssessmentQuestion[] } = {
@@ -14,31 +30,27 @@ export async function fetchQuestionsBasedOnEnv() {
   };
 
   try {
-    if (process.env.AIRTABLE_SUBMISSIONS_BASE && process.env.AIRTABLE_API_KEY) {
-      Airtable.configure({
-        apiKey: process.env.AIRTABLE_API_KEY,
-        apiVersion: undefined,
-        endpointUrl: undefined,
-        noRetryIfRateLimited: undefined,
-      });
-
-      const airtable = new AirtableStore(process.env.AIRTABLE_API_KEY);
+    if (process.env.AIRTABLE_SUBMISSIONS_BASE) {
+      const airtable = new AirtableStore(process.env.AIRTABLE_SUBMISSIONS_BASE);
       questions = await airtable.fetchQuestions();
       console.log(
         "Airtable key specified and questions exist in base. Using questions from Airtable."
       );
     } else {
+      console.log(
+        "`questions` table does not exist in base. Using questions from JSON file"
+      );
       questions = jsonQuestions();
     }
   } catch (error) {
+    console.log("Airtable key not provided. Using questions from JSON file");
     questions = jsonQuestions();
   }
-
+  _QUESTIONS = questions;
   return questions;
 }
 
 function jsonQuestions() {
-  console.log("Airtable key not provided. Using questions from JSON file");
   const questions = QuestionsData as {
     [type in RoomTypes]: RoomAssessmentQuestion[];
   };
--- pages/api/home-assessment.ts
+++ pages/api/home-assessment.ts
@@ -1,14 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Airtable from "airtable";
-import { AllRoomAssessmentQuestion } from "../../interfaces/home-assessment";
 
-import QuestionsData from "../../data/kingston/questions.json";
 import KingstonBylawMultiplexer from "../../data/kingston/bylawMultiplexer.json";
 import { handleHomeAssessment } from "../../api/handleHomeAssessment/handleHomeAssessment";
 import { ApiBylawMultiplexer } from "../../interfaces/api-home-assessment";
 import { AirtableStore } from "../../api/datastore/Airtable";
 import { Datastore } from "../../api/datastore/Datastore";
 import { MockDatastore } from "../../api/datastore/MockDatastore";
+import { getStaticQuestions } from "../../api/utils/fetchQuestionsBasedOnEnv";
 
 Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY,
@@ -17,9 +16,10 @@ Airtable.configure({
   noRetryIfRateLimited: undefined,
 });
 
-function curriedHandler(req: NextApiRequest, res: NextApiResponse) {
-  const questions = QuestionsData as AllRoomAssessmentQuestion;
+async function curriedHandler(req: NextApiRequest, res: NextApiResponse) {
   const multiplexer = KingstonBylawMultiplexer as ApiBylawMultiplexer;
+  const questions = await getStaticQuestions();
+
   let datastore: Datastore;
   if (process.env.AIRTABLE_SUBMISSIONS_BASE && process.env.AIRTABLE_API_KEY) {
     datastore = new AirtableStore(process.env.AIRTABLE_SUBMISSIONS_BASE);
@@ -27,6 +27,7 @@ function curriedHandler(req: NextApiRequest, res: NextApiResponse) {
     console.warn("Airtable base not provided, defaulting to mock datastore");
     datastore = new MockDatastore();
   }
+
   return handleHomeAssessment(req, res, multiplexer, questions, datastore);
 }
 
