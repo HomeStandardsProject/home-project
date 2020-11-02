2c044e738584b91599bd49c8f8ce0360f7f4f4c5
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-11-02 09:14:31 -0500

    fix type error

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+function handler(_: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json({ success: true });
 }
 
