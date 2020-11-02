fafbd932ec761ad22cbc6643acacf38f095a1ab1
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-11-02 09:45:29 -0500

    remove test api route

--- pages/api/test.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-function handler(_: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ success: true });
-}
-
-export default handler;
