94f44a841c0e62706baac02803e059e7022b8473
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-12-24 15:09:19 -0500

    update pdf design

--- components/resullts/AssessmentPDF.tsx
+++ components/resullts/AssessmentPDF.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { Document, Page, View, Text, StyleSheet } from "@react-pdf/renderer";
+import {
+  Document,
+  Page,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Link,
+} from "@react-pdf/renderer";
 import {
   ApiHomeAssessmentResult,
   ApiRoomAssessmentResult,
@@ -15,18 +23,22 @@ const styles = StyleSheet.create({
     marginTop: 4,
   },
   detailsSection: {
-    margin: 10,
+    margin: 16,
     padding: 8,
-    backgroundColor: "#edf2f7",
+    backgroundColor: "#3182ce",
     borderRadius: 4,
+  },
+  h1: {
+    fontSize: "12pt",
+    color: "white",
   },
   h2: {
     fontSize: "13pt",
-    fontWeight: "bold",
+    fontFamily: "Helvetica-Bold",
   },
   h3: {
     fontSize: "12pt",
-    fontWeight: "bold",
+    fontFamily: "Helvetica-Oblique",
   },
   bodyText: {
     fontSize: "11pt",
@@ -38,7 +50,10 @@ const styles = StyleSheet.create({
   },
   bodyCommentText: {
     fontSize: "11pt",
-    fontStyle: "italic",
+  },
+  landlord: {
+    fontSize: "10pt",
+    color: "white",
   },
   commentContainer: {
     marginTop: 4,
@@ -56,6 +71,20 @@ const styles = StyleSheet.create({
     fontSize: "8pt",
     margin: 10,
     marginBottom: 0,
+  },
+  logo: {
+    width: "150px",
+    marginLeft: 16,
+  },
+  violationsLabel: {
+    fontSize: "11pt",
+    color: "#c53031",
+    fontFamily: "Helvetica-Bold",
+  },
+  possibleViolationsLabel: {
+    fontSize: "11pt",
+    color: "#2b6cb0",
+    fontFamily: "Helvetica-Bold",
   },
 });
 
@@ -77,14 +106,17 @@ export const AssessmentPDF = ({
     <Document>
       <Page size="A4" style={styles.page}>
         <Text style={styles.generatedDate}>{generatedDate}</Text>
+        <Link src="https://homeproject.qbacc.org">
+          <Image src="/logo.png" style={styles.logo} />
+        </Link>
         <View style={styles.detailsSection}>
-          <Text style={styles.h2}>
+          <Text style={styles.h1}>
             {result.details.address.formatted}{" "}
             {result.details.unitNumber
               ? `(Unit ${result.details.unitNumber})`
               : ``}
           </Text>
-          <Text style={styles.bodyCommentText}>{landlord}</Text>
+          <Text style={styles.landlord}>{landlord}</Text>
         </View>
         <View style={styles.section}>{result.rooms.map(roomRenderer)}</View>
       </Page>
@@ -95,8 +127,30 @@ export const AssessmentPDF = ({
 const roomRenderer = (room: ApiRoomAssessmentResult) => (
   <View key={room.id} style={styles.roomContainer}>
     <Text style={styles.h2}>{room.name}</Text>
-
+    {room.violations.length > 0 && (
+      <Text style={styles.violationsLabel}>
+        {room.violations.length} Violations
+      </Text>
+    )}
     {room.violations.map((violation, i) => (
+      <View key={i} style={styles.violation}>
+        <Text style={styles.h3}>{violation.name}</Text>
+        <Text style={styles.bodyText}>{violation.description}</Text>
+        <View style={styles.commentContainer}>
+          {violation.userProvidedDescriptions.map((comment, i) => (
+            <View key={i} style={styles.bodyComment}>
+              <Text style={styles.bodyCommentText}>{comment}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    ))}
+    {room.possibleViolations.length > 0 && (
+      <Text style={styles.possibleViolationsLabel}>
+        {room.possibleViolations.length} Possible Violations
+      </Text>
+    )}
+    {room.possibleViolations.map((violation, i) => (
       <View key={i} style={styles.violation}>
         <Text style={styles.h3}>{violation.name}</Text>
         <Text style={styles.bodyText}>{violation.description}</Text>
